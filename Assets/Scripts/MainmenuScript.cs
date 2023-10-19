7a702b0ef14bb36249cdfc2e91490477f68cbb18
DevDen94 <72824280+DevDen94@users.noreply.github.com>
2023-10-19 10:32:04 +0500

    Sounds and Music

--- Assets/Scripts/MainmenuScript.cs
+++ Assets/Scripts/MainmenuScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainmenuScript : MonoBehaviour
@@ -10,6 +11,7 @@ public class MainmenuScript : MonoBehaviour
     [SerializeField] GameObject MainMenuPanel;
     [SerializeField] GameObject MonsterSelectPanel;
     [SerializeField] GameObject SettingsSelectPanel;
+    [SerializeField] Text CoinText;
     
     public bool Sound;
     
@@ -21,10 +23,12 @@ public class MainmenuScript : MonoBehaviour
 
 
 
+
     // Start is called before the first frame update
     void Start()
     {
         Time.timeScale = 1;
+        CoinText.text = ""+PlayerPrefs.GetInt("Coins",0);
     }
 
     // Update is called once per frame
@@ -41,18 +45,24 @@ public class MainmenuScript : MonoBehaviour
 
     public void MainMenuPanelButton()
     {
-
+        MainMenuPanel.SetActive(true);
+        MonsterSelectPanel.SetActive(false);
+        SettingsSelectPanel.SetActive(false);
     }
 
 
     public void MonsterSelectPanelButton()
     {
-
+        MainMenuPanel.SetActive(false);
+        MonsterSelectPanel.SetActive(true);
+        SettingsSelectPanel.SetActive(false);
     }
 
     public void SettingsPanelButton()
     {
-
+        MainMenuPanel.SetActive(false);
+        MonsterSelectPanel.SetActive(false);
+        SettingsSelectPanel.SetActive(true);
 
     }
 
